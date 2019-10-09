b15a8cd101fb93beb331f3b71478819a989fa0d0
Hisashi Miura <hisashi.office@gmail.com>
2019-10-09 12:44:22 +0900

    追加：η値の計算

--- Basis/simple_envelope_evaluation_monte_carlo/calc_designed_indices.py
+++ Basis/simple_envelope_evaluation_monte_carlo/calc_designed_indices.py
@@ -1,9 +1,28 @@
+from typing import List, Tuple
+
 from nu_value import get_nu_c
+from f_value import get_f, Sunshade, get_f_value
+
+
+def get_direction(main_direction: str) -> Tuple[str, str, str, str]:
+    """
+    Args:
+        main_direction: 主方位
+    Returns:
+        主方位、主方位＋90°、主方位＋180°、主方位＋270°　の方位（タプル）
+    """
+
+    directions = ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']
+
+    idx = directions.index(main_direction)
+
+    return directions[idx], directions[idx-6], directions[idx-4], directions[idx-2]
 
 
 def calc_designed_indices(
         model_house: dict,
         region: int,
+        main_direction: str,
         upper_u: float,
         wall_u: float,
         lower_u: float,
@@ -12,12 +31,14 @@ def calc_designed_indices(
         window_u: float,
         window_attached_u: float,
         window_eta: float,
-        window_attached_eta: float
+        window_attached_eta: float,
+        l_value: float
 ):
     """
     Args:
         model_house: モデルハウス（面積等）の辞書
         region: 地域の区分
+        main_direction: 主方位
         upper_u: 上部のU値, W/m2K
         wall_u: 外壁のU値, W/m2K
         lower_u: 下部のU値, W/m2K
@@ -27,6 +48,7 @@ def calc_designed_indices(
         window_attached_u: 窓（障子）のU値, U値, W/m2K
         window_eta: 窓のη値
         window_attached_eta: 窓（障子）のη値
+        l_value: 日除けのl_value
     Returns:
 
     """
@@ -67,9 +89,94 @@ def calc_designed_indices(
         + sum(a_evp_door) * window_u\
         + sum(a_evp_wall) * wall_u
 
-    m_c = a_evp_roof * upper_u * 0.034 * get_nu_c(region=region, direction='top')
+    d0, d90, d180, d270 = get_direction(main_direction)
+
+    f0 = get_f_value(glass_type='Type1', season='Cooling', region=region, direction=d0, l_value=l_value)
+    f90 = get_f_value(glass_type='Type1', season='Cooling', region=region, direction=d90, l_value=l_value)
+    f180 = get_f_value(glass_type='Type1', season='Cooling', region=region, direction=d180, l_value=l_value)
+    f270 = get_f_value(glass_type='Type1', season='Cooling', region=region, direction=d270, l_value=l_value)
+
+    m_c = a_evp_roof * upper_u * 0.034 * get_nu_c(region=region, direction='top')\
+        + a_evp_window[0] * window_eta * f0 * get_nu_c(region=region, direction=d0)\
+        + a_evp_window[1] * window_attached_eta * f90 * get_nu_c(region=region, direction=d90)\
+        + a_evp_window[2] * window_eta * f180 * get_nu_c(region=region, direction=d180)\
+        + a_evp_window[3] * window_eta * f270 * get_nu_c(region=region, direction=d270)\
+        + a_evp_door[0] * window_u * 0.034 * get_nu_c(region=region, direction=d0)\
+        + a_evp_door[1] * window_u * 0.034 * get_nu_c(region=region, direction=d90)\
+        + a_evp_door[2] * window_u * 0.034 * get_nu_c(region=region, direction=d180)\
+        + a_evp_door[3] * window_u * 0.034 * get_nu_c(region=region, direction=d270) \
+        + a_evp_wall[0] * wall_u * 0.034 * get_nu_c(region=region, direction=d0)\
+        + a_evp_wall[1] * wall_u * 0.034 * get_nu_c(region=region, direction=d90)\
+        + a_evp_wall[2] * wall_u * 0.034 * get_nu_c(region=region, direction=d180)\
+        + a_evp_wall[3] * wall_u * 0.034 * get_nu_c(region=region, direction=d270)
 
     u_a = q / a_evp_total
 
-    return u_a
+    eta_a_c = m_c / a_evp_total
+
+    return u_a, eta_a_c
+
+
+if __name__ == '__main__':
+
+    model_house = {
+        'house_type': 'detached',
+        'floor_ins_type': 'floor',
+        'bath_ins_type': 'base',
+        'a_f': [50.847457627118644, 39.152542372881356],
+        'a_evp_ef_etrc': 2.4843,
+        'a_evp_f_etrc': 0.0,
+        'l_base_etrc_outside': (0.0, 1.82, 1.365, 0.0),
+        'l_base_etrc_inside': 3.185,
+        'a_evp_ef_bath': 3.3124000000000002,
+        'a_evp_f_bath': 0.0,
+        'l_base_bath_outside': (0.0, 1.82, 1.82, 0.0),
+        'l_base_bath_inside': 3.64,
+        'f_s': [1.08, 1.04],
+        'l_prm': [30.80479821749104, 26.029948852968104],
+        'l_ms': [10.609982280729707, 8.29490083566609],
+        'l_os': [4.7924168280158135, 4.720073590817963],
+        'a_evp_ef_other': 0.0,
+        'a_evp_ef_total': 5.7967,
+        'a_evp_f_other': 45.05075762711864,
+        'a_evp_f_total': 45.05075762711864,
+        'l_base_other_outside': (0.0, 0.0, 0.0, 0.0),
+        'l_base_other_inside': 0.0,
+        'l_base_total_outside': (0.0, 3.64, 3.185, 0.0),
+        'l_base_total_inside': 6.825,
+        'a_evp_roof': 50.847457627118644,
+        'a_evp_base_etrc_outside': (0.0, 0.3276, 0.2457, 0.0),
+        'a_evp_base_etrc_inside': 0.5733,
+        'a_evp_base_bath_outside': (0.0, 0.91, 0.91, 0.0),
+        'a_evp_base_bath_inside': 1.82,
+        'a_evp_base_other_outside': (0.0, 0.0, 0.0, 0.0),
+        'a_evp_base_other_inside': 0.0,
+        'a_evp_base_total_outside': (0.0, 1.2376, 1.1557, 0.0),
+        'a_evp_base_total_inside': 2.3933,
+        'a_evp_srf': ([30.768948614116148, 22.396232256298443], [13.898008801245858, 12.7441986952085], [30.768948614116148, 22.396232256298443], [13.898008801245858, 12.7441986952085]),
+        'a_evp_total_not_base': 261.30969198797516,
+        'a_evp_total': 266.0962919879752,
+        'a_evp_open_total': 36.58335687831652,
+        'a_evp_window_total': 33.073356878316524,
+        'a_evp_window': (22.6949374899008, 2.3845890309266213, 3.628147249551323, 4.365683107937781),
+        'a_evp_door': (0.0, 1.89, 1.62, 0.0),
+        'a_evp_wall': (30.47024338051379, 22.367618465527734, 47.917033620863265, 22.276524388516577)
+    }
+
+    u_a = calc_designed_indices(
+        model_house=model_house,
+        region=6,
+        main_direction='sw',
+        upper_u=0.1,
+        wall_u=0.1,
+        lower_u=0.1,
+        base_psi_outside=0.1,
+        base_psi_inside=0.1,
+        window_u=0.1,
+        window_attached_u=0.1,
+        window_eta=0.1,
+        window_attached_eta=0.1,
+        l_value=1/0.3)
+
+    print(u_a)
 
--- Basis/simple_envelope_evaluation_monte_carlo/f_value.py
+++ Basis/simple_envelope_evaluation_monte_carlo/f_value.py
@@ -1,40 +1,224 @@
 import numpy as np
 import pandas as pd
-from  scipy.interpolate  import  interp1d
+from scipy.interpolate  import  interp1d
 import itertools
+from functools import lru_cache
+
+from collections import namedtuple
+from typing import Optional, List, Dict, Union, Tuple
 
 
-class f1f2Value:
+# obtain the list from csv data
+def get_f_list():
 
-    # obtain the list from csv data
-    def __init__(self):
-        self.f_list = pd.read_csv('f_value.csv', skiprows = 1)
-        self.f_list['ガラス仕様の区分'] = self.f_list['ガラス仕様の区分'].fillna(method='ffill')
-        self.f_list['期間']             = self.f_list['期間']            .fillna(method='ffill')
-        self.f_list.columns=['Glass Spec',
-                             'Season',
-                             'l_value',
-                             '1N','1NE','1E','1SE','1S','1SW','1W','1NW',
-                             '2N','2NE','2E','2SE','2S','2SW','2W','2NW',
-                             '3N','3NE','3E','3SE','3S','3SW','3W','3NW',
-                             '4N','4NE','4E','4SE','4S','4SW','4W','4NW',
-                             '5N','5NE','5E','5SE','5S','5SW','5W','5NW',
-                             '6N','6NE','6E','6SE','6S','6SW','6W','6NW',
-                             '7N','7NE','7E','7SE','7S','7SW','7W','7NW',
-                             '8N','8NE','8E','8SE','8S','8SW','8W','8NW']
-        self.f_list['Season'] = self.f_list['Season'].replace( { '冷房':'Cooling', '暖房':'Heating' } )
-        self.f_list['Glass Spec'] = self.f_list['Glass Spec'].replace( { 1:'Type1', 2:'Type2', 3:'Type3', 4:'Type4', 5:'Type5', 6:'Type6', 7:'Type7' } )
+    f_list = pd.read_csv('f_value.csv', skiprows = 1)
+    f_list['ガラス仕様の区分'] = f_list['ガラス仕様の区分'].fillna(method='ffill')
+    f_list['期間'] = f_list['期間'].fillna(method='ffill')
+    f_list.columns=['Glass Spec',
+                    'Season',
+                    'l_value',
+                    '1N','1NE','1E','1SE','1S','1SW','1W','1NW',
+                    '2N','2NE','2E','2SE','2S','2SW','2W','2NW',
+                    '3N','3NE','3E','3SE','3S','3SW','3W','3NW',
+                    '4N','4NE','4E','4SE','4S','4SW','4W','4NW',
+                    '5N','5NE','5E','5SE','5S','5SW','5W','5NW',
+                    '6N','6NE','6E','6SE','6S','6SW','6W','6NW',
+                    '7N','7NE','7E','7SE','7S','7SW','7W','7NW',
+                    '8N','8NE','8E','8SE','8S','8SW','8W','8NW']
+    f_list['Season'] = f_list['Season'].replace( { '冷房':'Cooling', '暖房':'Heating' } )
+    f_list['Glass Spec'] = f_list['Glass Spec'].replace( { 1:'Type1', 2:'Type2', 3:'Type3', 4:'Type4', 5:'Type5', 6:'Type6', 7:'Type7' } )
 
-    # get l and nu list in table
-    def getTableValue(self, glassType, season, region, direction):
-        r = str({ 'region1':1, 'region2':2, 'region3':3, 'region4':4, 'region5':5, 'region6':6, 'region7':7, 'region8':8 }[region])
-        d = { 'N':'N', 'NE':'NE', 'E':'E', 'SE':'SE', 'S':'S', 'SW':'SW', 'W':'W', 'NW':'NW' }[direction]
-        return self.f_list[ (self.f_list['Season']==season) & (self.f_list['Glass Spec']==glassType) ].loc[:,['l_value',r+d]]
+    return f_list
 
-    # get f1 or f2 value
-    def getValue(self, glassType, season, region, direction, lValue):
-        if lValue > 20.0:
-            lValue = 20.0
-        dat = self.getTableValue(glassType, season, region, direction)
-        return float( interp1d(dat.iloc[:,0],dat.iloc[:,1])(lValue) )
+# get l and nu list in table
+def get_table_value(glass_type: str, season: str, region: int, direction: str):
+
+    r = str(region)
+
+    d = {
+        'n': 'N',
+        'ne': 'NE',
+        'e': 'E',
+        'se': 'SE',
+        's': 'S',
+        'sw': 'SW',
+        'w': 'W',
+        'nw': 'NW'
+    }[direction]
+
+    f_list = get_f_list()
+
+    return f_list[(f_list['Season'] == season) & (f_list['Glass Spec'] == glass_type)].loc[:, ['l_value', r + d]]
+
+
+# get f1 or f2 value
+@lru_cache(maxsize=None)
+def get_f_value(glass_type: str, season: str, region: int, direction: str, l_value: float) -> float:
+
+    if l_value > 20.0:
+        l_value = 20.0
+
+    dat = get_table_value(glass_type, season, region, direction)
+
+    return float(interp1d(dat.iloc[:, 0], dat.iloc[:, 1])(l_value))
+
+
+Sunshade = namedtuple('Sunshade', [
+    'existence',
+    'input_method',
+    'depth',
+    'd_h',
+    'd_e',
+    'x1',
+    'x2',
+    'x3',
+    'y1',
+    'y2',
+    'y3',
+    'z_x_pls',
+    'z_x_mns',
+    'z_y_pls',
+    'z_y_mns',
+])
+
+
+def get_f_not_input(season: str, region: int) -> Optional[float]:
+    """
+    Args:
+        season: 期間
+        region: 地域の区分
+    Returns:
+        f値
+    """
+
+    if season == 'heating':
+        if region == 8:
+            return None
+        else:
+            return 0.51
+    elif season == 'cooling':
+        return 0.93
+    else:
+        raise ValueError()
+
+
+def get_f(season: str, region: int, direction: str, sunshade: Optional[Sunshade]) -> Optional[float]:
+    """
+    Args:
+        season: 期間
+        region: 地域の区分
+        direction: 方位
+        sunshade: 日除けの形状
+    Returns:
+        f値
+    """
+
+    if sunshade is None:
+        return get_f_not_input(season, region)
+    elif sunshade.existence:
+        return get_f_existence(season, region, direction, sunshade)
+    elif not sunshade.existence:
+        return get_f_not_existence(season, region, direction)
+    else:
+        raise ValueError()
+
+
+def get_f_existence(season: str, region: int, direction: str, sunshade: Sunshade) -> Optional[float]:
+    """
+    Args:
+        season: 期間
+        region: 地域の区分
+        direction: 方位
+        sunshade: 日除けの形状
+    Returns:
+        f値
+    """
+
+    if sunshade.input_method == 'simple':
+        if season == 'heating':
+            if region == 8:
+                return None
+            else:
+                if direction == 'sw' or direction == 's' or direction == 'se':
+                    return min(0.01 * (5 + 20 * (3 * sunshade.d_e + sunshade.d_h)/sunshade.depth), 0.72)
+                elif (direction == 'n' or direction == 'ne' or direction == 'e'
+                      or direction == 'nw' or direction == 'w'):
+                    return min(0.01 * (10 + 15 * (2 * sunshade.d_e + sunshade.d_h) / sunshade.depth), 0.72)
+                elif direction == 'top':
+                    return 1.0
+                elif direction == 'bottom':
+                    return 0.0
+                else:
+                    raise ValueError()
+        elif season == 'cooling':
+            if region == 8:
+                if direction == 'sw' or direction == 's' or direction == 'se':
+                    return min(0.01 * (16 + 19 * (2 * sunshade.d_e + sunshade.d_h)/sunshade.depth), 0.93)
+                elif (direction == 'n' or direction == 'ne' or direction == 'e'
+                      or direction == 'nw' or direction == 'w'):
+                    return min(0.01 * (16 + 24 * (2 * sunshade.d_e + sunshade.d_h) / sunshade.depth), 0.93)
+                elif direction == 'top':
+                    return 1.0
+                elif direction == 'bottom':
+                    return 0.0
+                else:
+                    raise ValueError()
+            else:
+                if direction == 's':
+                    return min(0.01 * (24 + 9 * (3 * sunshade.d_e + sunshade.d_h)/sunshade.depth), 0.93)
+                elif (direction == 'n' or direction == 'ne' or direction == 'e' or direction == 'se'
+                      or direction == 'nw' or direction == 'w' or direction == 'sw'):
+                    return min(0.01 * (16 + 24 * (2 * sunshade.d_e + sunshade.d_h) / sunshade.depth), 0.93)
+                elif direction == 'top':
+                    return 1.0
+                elif direction == 'bottom':
+                    return 0.0
+                else:
+                    raise ValueError()
+        else:
+            raise ValueError()
+    elif sunshade.input_method == 'detailed':
+        raise NotImplementedError()
+    else:
+        raise ValueError()
+
+
+def get_f_not_existence(season: str, region: int, direction: str) -> Optional[float]:
+    """
+    Args:
+        season: 期間
+        region: 地域の区分
+        direction: 方位
+    Returns:
+        f値
+    Notes:
+        第3章第4部「日射熱取得率」における表１・表２のうち、ガラスの仕様の区分1の値を採用した。
+    """
+
+    return {
+        'heating': {
+            'top': {1: 0.90, 2: 0.91, 3: 0.91, 4: 0.91, 5: 0.90, 6: 0.90, 7: 0.90, 8: None}[region],
+            'n': {1: 0.862, 2: 0.860, 3: 0.862, 4: 0.861, 5: 0.867, 6: 0.870, 7: 0.873, 8: None}[region],
+            'ne': {1: 0.848, 2: 0.851, 3: 0.850, 4: 0.846, 5: 0.838, 6: 0.839, 7: 0.833, 8: None}[region],
+            'e': {1: 0.871, 2: 0.873, 3: 0.869, 4: 0.874, 5: 0.874, 6: 0.874, 7: 0.868, 8: None}[region],
+            'se': {1: 0.892, 2: 0.888, 3: 0.885, 4: 0.883, 5: 0.894, 6: 0.896, 7: 0.892, 8: None}[region],
+            's': {1: 0.892, 2: 0.880, 3: 0.884, 4: 0.874, 5: 0.894, 6: 0.889, 7: 0.896, 8: None}[region],
+            'sw': {1: 0.888, 2: 0.885, 3: 0.885, 4: 0.882, 5: 0.891, 6: 0.885, 7: 0.894, 8: None}[region],
+            'w': {1: 0.869, 2: 0.874, 3: 0.871, 4: 0.872, 5: 0.871, 6: 0.874, 7: 0.870, 8: None}[region],
+            'nw': {1: 0.850, 2: 0.850, 3: 0.850, 4: 0.845, 5: 0.840, 6: 0.844, 7: 0.834, 8: None}[region],
+            'bottom': {1: 0.0, 2: 0.0, 3: 0.0, 4: 0.0, 5: 0.0, 6: 0.0, 7: 0.0, 8: None}[region],
+        }[direction],
+        'cooling': {
+            'top': {1: 0.93, 2: 0.93, 3: 0.93, 4: 0.94, 5: 0.93, 6: 0.94, 7: 0.94, 8: 0.93}[region],
+            'n': {1: 0.853, 2: 0.857, 3: 0.853, 4: 0.852, 5: 0.860, 6: 0.847, 7: 0.838, 8: 0.848}[region],
+            'ne': {1: 0.865, 2: 0.864, 3: 0.862, 4: 0.861, 5: 0.863, 6: 0.862, 7: 0.861, 8: 0.857}[region],
+            'e': {1: 0.882, 2: 0.877, 3: 0.870, 4: 0.881, 5: 0.874, 6: 0.880, 7: 0.881, 8: 0.877}[region],
+            'se': {1: 0.864, 2: 0.858, 3: 0.853, 4: 0.853, 5: 0.854, 6: 0.852, 7: 0.849, 8: 0.860}[region],
+            's': {1: 0.807, 2: 0.812, 3: 0.799, 4: 0.784, 5: 0.807, 6: 0.795, 7: 0.788, 8: 0.824}[region],
+            'sw': {1: 0.860, 2: 0.861, 3: 0.859, 4: 0.850, 5: 0.858, 6: 0.852, 7: 0.847, 8: 0.858}[region],
+            'w': {1: 0.880, 2: 0.878, 3: 0.883, 4: 0.876, 5: 0.875, 6: 0.880, 7: 0.880, 8: 0.876}[region],
+            'nw': {1: 0.866, 2: 0.864, 3: 0.865, 4: 0.861, 5: 0.862, 6: 0.864, 7: 0.862, 8: 0.859}[region],
+            'bottom': 0.0,
+        }[direction],
+    }[season]
 
